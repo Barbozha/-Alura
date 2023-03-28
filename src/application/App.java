59542e707511f5bc882f1020dde63651ec46375b
Barbozha <barbozha@gmail.com>
2023-03-28 08:43:49 -0300

    Versão com estrelinha

--- src/application/App.java
+++ src/application/App.java
@@ -11,6 +11,8 @@ import java.util.Map;
 public class App {
 
 	public static void main(String[] args) throws Exception {
+		
+		// FAZ A CONEXÃƒO COM O SERVIDOR IMDB
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		URI endereco = URI.create(url);
 		HttpClient client = HttpClient.newHttpClient();
@@ -18,17 +20,25 @@ public class App {
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		String body = response.body();
 		System.out.println(body);
+		double classificacao = 0;
+		int num;
 		
-		// extrair só os dados que interessam (título, poster, classificação)
+		// EXTRAI OS DADOS QUE INTERESSAM (NOME DO FILME, CLASSIFICAÃ‡ÃƒO, DADOS DA IMAGEM DO FILME)
 		JsonParser parser = new JsonParser();
 		List<Map<String, String>> listaDeFilmes = parser.parse(body);
 	
 		
-		// exibir e manipular os dados 
+		// EXIBE OS DADOS ORIUNDO DA API DO IMDB 
 		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println("Título: "+filme.get("title"));
-			System.out.println("Imagem: "+filme.get("image"));
-			System.out.println("Classificação: "+filme.get("imDbRating"));
+			System.out.println("TÃ­tulo: "+filme.get("title"));
+			System.out.println("Imagem de capa: "+filme.get("image"));
+			System.out.println("ClassificaÃ§Ã£o: "+filme.get("imDbRating"));
+			classificacao = Double.parseDouble(filme.get("imDbRating"));
+			num = (int) classificacao;
+			for(int i = 1; i <= num ; i++) {
+				System.out.print("â­");
+			}
+			System.out.println();
 			System.out.println();
 		}
 
